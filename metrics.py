05fb1bddf9ea7b02ed27bc266cc5378073aace74
antani-lab <128553312+antani-lab@users.noreply.github.com>
2026-08-27 16:17:19 -0400

    Delete metrics.py

--- metrics.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from sklearn.metrics import f1_score
-
-
-def weighted_f1(y_true, y_pred):
-    value = float(f1_score(y_true=y_true, y_pred=y_pred, average="weighted"))
-    return value
